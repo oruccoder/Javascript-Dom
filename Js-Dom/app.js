5fe535983974f31366cac9be8a485be6fb83bc76
= <ramazanovruc@gmail.com>
2026-04-17 10:48:32 +0400

    Javascript DOM

---
+++ Js-Dom/app.js
@@ -0,0 +1,24 @@
+// Javascript dom
+
+
+
+// console.log(window);
+
+// console.log(document);
+
+
+// value = document.location.href;
+// value = document.location.hostname;
+// value = document.location.port;
+// value = document.location.pathname;
+
+
+// value = document.characterSet;
+// value = document.title;
+
+// value = document.links.item(1).id;
+// value = document.links.item(3).getAttribute("id")
+// value = document.links.item(3).getAttribute("class")
+value = document.links.item(3).classList;
+
+console.log(value);
