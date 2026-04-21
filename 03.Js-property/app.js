f1dc651092706ee7ffcc2209e8ea4f7018853dee
= <ramazanovruc@gmail.com>
2026-04-21 11:47:53 +0400

    Javascript Dom Css Property

---
+++ 03.Js-property/app.js
@@ -0,0 +1,10 @@
+const todo = document.querySelectorAll(".lists")[0]
+const text = todo.querySelector("span")
+
+// const text = document.querySelector("span")
+
+// text.style.color = "red";
+
+
+todo.style.color = "red";
+todo.style.backgroundColor = "purple"
