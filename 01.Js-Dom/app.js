9b73806a41f8fd16f821955faa8db5f98864d2b2
= <ramazanovruc@gmail.com>
2026-04-21 11:45:18 +0400

    Js Dom Updated

---
+++ 01.Js-Dom/app.js
@@ -0,0 +1,60 @@
+// Javascript dom
+
+
+
+// console.log(window);
+
+// console.log(document);
+
+
+// value = document.location.href;
+// value = document.location.hostname;
+// value = document.location.port;
+// value = document.location.pathname;
+
+
+// value = document.characterSet;
+// value = document.title;
+
+// value = document.links.item(1).id;
+// value = document.links.item(3).getAttribute("id")
+// value = document.links.item(3).getAttribute("class")
+// value = document.links.item(3).classList;
+
+
+// console.log(value);
+
+
+
+// ---------------------------------------------
+
+// value = document.forms;
+// value = document.forms.item(1).method;
+// value = document.forms.item(1).id;
+// value = document.forms.item(1).children;
+
+// value = document.domain;
+
+
+value = document.contentType;
+console.log(value);
+
+
+
+
+
+
+
+
+// ! Tapşırıq-----------------------
+
+// let str = "a1b2c3d4";
+// let result = "";
+
+// for(let i = 0; i<str.length; i++){
+//     if(!isNaN(str[i]) && str[i] !== ""){
+//         result += str[i]
+//     }
+// }
+
+// console.log(result);
