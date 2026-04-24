65447008fa23dcd66fd0ff1aea50553726f839c6
= <ramazanovruc@gmail.com>
2026-04-24 11:02:29 +0400

    Delete Element

---
+++ 06.DeleteElement/main.js
@@ -0,0 +1,13 @@
+// Element Silmək
+
+
+const todoList = document.querySelector("#list-group");
+const todos = document.querySelectorAll(".lists")
+
+// todos[0].remove();
+// todos[1].remove();
+
+todos[todos.length -1].remove();
+
+
+console.log(todos);
