a0f4e356e556617240f04393c22aedbfb131fffb
= <ramazanovruc@gmail.com>
2026-04-28 10:10:02 +0400

    Updated Delete Element

--- 06.DeleteElement/main.js
+++ 06.DeleteElement/main.js
@@ -3,11 +3,20 @@
 
 const todoList = document.querySelector("#list-group");
 const todos = document.querySelectorAll(".lists")
+const todo1 = document.querySelector(".lists")
 
 // todos[0].remove();
 // todos[1].remove();
 
-todos[todos.length -1].remove();
+// todos[todos.length -1].remove();
+
 
 
 console.log(todos);
+
+
+// todo1.remove();
+
+// todoList.removeChild(todos[0]);
+// todoList.removeChild(todos[todos.length -1])
+todoList.removeChild(todoList.lastElementChild)
