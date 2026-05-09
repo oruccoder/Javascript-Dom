9d69c7939e59bc96335f08be5a901798b43e39ad
= <ramazanovruc@gmail.com>
2026-05-09 11:00:01 +0400

    Local Storage

---
+++ 13.LocalStorage/app.js
@@ -0,0 +1,39 @@
+//  Local Storage
+
+
+//  Dəyər əlavə etmək
+
+// localStorage.setItem("1", "Bmw");
+// localStorage.setItem("2", "Mercedes");
+// localStorage.setItem("3", "Audi");
+// localStorage.setItem("4", "Dodge");
+
+// Dəyər almaq
+
+
+// let value = localStorage.getItem("4");
+
+// console.log(value);
+
+
+// Dəyər Silmək
+
+// localStorage.removeItem("1")
+
+// Hamısını silmək
+
+// localStorage.clear();
+
+// let cars = ["Bmw", "Mercedes", "Audi", "Dodge"];
+
+// localStorage.setItem("cars", JSON.stringify(cars))
+
+// let value = JSON.parse(localStorage.getItem("cars"))
+
+// value.forEach(function(car){
+//     console.log(car);
+    
+// })
+
+
+
