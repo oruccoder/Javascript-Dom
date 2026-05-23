089cfb9ec3fb2561abfd7f7766bf926e1e1e42ca
= <ramazanovruc@gmail.com>
2026-05-23 10:36:11 +0400

    ES6 Arrow Function

---
+++ 15.ArrowFunction/app.js
@@ -0,0 +1,49 @@
+// Arrow Function
+
+// function yazdir(){
+//     console.log("Hello World");
+// }
+
+// yazdir()
+
+
+// const yazdir = () =>{
+//     console.log("Arrow Function");
+// }
+
+// yazdir()
+
+// const yazdir = (firstName, lastName) => {
+//     console.log("Salam", firstName,lastName);
+// }
+
+// const yazdir = (firstName, lastName) => console.log("Salam", firstName,lastName);
+
+// yazdir("Oruj", "Ramazanov");
+
+
+// const yazdir = (firstName,lastName) => {
+//     console.log("Salam", firstName, lastName);
+//     let a = 5;
+//     console.log(a);
+//     console.log("Javascript");
+    
+// }
+
+// yazdir("Oruj", "Ramazanov")
+
+
+// const yazdir = firstName => {
+//     console.log("Salam", firstName);
+// }
+
+// yazdir("Oruj")
+
+// const kubAl = (x) => {
+//     return x*x*x;
+// }
+
+const kubAl = (x) => x*x*x;
+
+
+console.log("Value ", kubAl(2));
