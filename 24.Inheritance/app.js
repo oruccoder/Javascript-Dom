75685d015e423e96157b533d3c40df7f0877b06d
= <ramazanovruc@gmail.com>
2026-06-09 12:04:44 +0400

    Inheritance (OOP)

---
+++ 24.Inheritance/app.js
@@ -0,0 +1,24 @@
+//  Inheritance(extends)
+
+// class Person{
+//     firstName = "Murad";
+
+//     write(){
+//         console.log("Person Write: ", this.firstName);
+//     }
+
+// }
+
+// const person = new Person();
+// person.write();
+
+
+// class Student extends Person{
+//     write(){
+//         console.log("Comes from Person Methods", this.firstName);
+//         super.write();
+//     }
+// }
+
+// const student1 = new Student();
+// student1.write();
