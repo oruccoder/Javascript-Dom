dcc8a8cb6275f507d05f7931556819f1de5c81c4
= <ramazanovruc@gmail.com>
2026-06-27 21:19:48 +0400

    Promise

---
+++ 27.AsynchronsProgramming/5.Promise/app.js
@@ -0,0 +1,79 @@
+// ? ----------- Promise ------------
+
+
+// let check = false;
+// const promise1 = new Promise((resolve,reject) => {
+//     if(check){
+//         resolve("Promise uğurludur")
+//     }
+//     else{
+//         reject("Promise uğursuzdur!")
+//     }
+// })
+
+// console.log(promise1);
+
+
+// let check = true;
+
+// function checkPromise(){
+//     return new Promise((resolve, reject) => {
+//         if(check){
+//             resolve("Promise uğurludur") // pending -> fulfilled
+//         }
+//         else{
+//             reject("Promise uğursuzdur!")
+//         }
+//     })
+// }
+
+// checkPromise()
+// .then((response) => {
+//     console.log(response);
+// })
+
+// .catch((err) => {
+//     console.log(err);
+// })
+
+// .finally(() => console.log("Hər zaman çalışar"));
+
+
+// Promise {
+//     state: "fulfilled",
+//     value: "Uğurlu"
+// }
+
+// Promise {
+//     state: "rejected",
+//     value: "uğursuz"
+// }
+
+// resolve(value)
+
+
+// ? ---------- Promise + XHMLHTTPREQUEST
+
+
+
+function readStudent(url){
+    return new Promise((resolve,reject) => {
+        const xhr = new XMLHttpRequest;
+        try {
+            xhr.addEventListener("readystatechange", () => {
+                if(xhr.readyState === 4 && xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                }
+            })
+        } catch (error) {
+            reject(error)
+        }
+
+        xhr.open("GET", url);
+        xhr.send();
+    })
+}
+
+readStudent("students.json")
+.then((data) => console.log(data))
+.catch((err) => console.log(err))
